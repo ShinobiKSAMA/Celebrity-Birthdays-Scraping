ac25e4b693ff397682267e6a4042f6cec54c7d83
ShinobiKSAMA <46086968+ShinobiKSAMA@users.noreply.github.com>
2019-02-05 15:22:03 +0530

    Add files via upload

---
+++ CelebBday.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def BDayRe(month, day):
+	url = "https://www.imdb.com/search/name?birth_monthday="+month+"-"+day+"&refine=birth_monthday&ref_=nv_cel_brn"
+	imdb = requests.get(url)
+	soup2 = BeautifulSoup(imdb.text, 'html.parser')
+	imlinks = soup2.findAll('h3', class_='lister-item-header', limit=25)
+	i = []
+	for Bday in imlinks:
+		i.append(re.sub(re.compile('<.*?>'), '', str(Bday)).replace('\n', ' '))
+	for p in i:
+		print(p)
+
+month = input("Enter the Month Number : ")
+day = input("Enter the Date : ")
+BDayRe(month, day)
